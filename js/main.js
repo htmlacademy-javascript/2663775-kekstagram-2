df4343a3a685e2c91d1375cf2d9a73d05654b3b1
Privalov94 <arprivalov110594@gmail.com>
2026-01-08 23:37:41 +0500

    добавляет валидацию формы

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { createPhotos } from './create-photos';
 import { renderMiniatures } from './thumbails';
+import './upload-thumbail-form.js';
+import './form-validation.js';
 
 const photos = createPhotos();
 renderMiniatures(photos);
